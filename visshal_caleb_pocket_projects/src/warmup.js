e3badf32a46d5976c8c56fb2b23a8e38ab1017da
Visshal Suresh <skvisshal@gmail.com>
2021-11-30 15:45:55 -0500

    phase 1

--- visshal_caleb_pocket_projects/src/warmup.js
+++ visshal_caleb_pocket_projects/src/warmup.js
@@ -2,7 +2,16 @@
 const partyHeader = document.getElementById('party');
 
 export const htmlGenerator = (string, htmlElement) => {
-
+    if (htmlElement.children) {
+        const array = Array.from(htmlElement.children)
+        for(let i = 0; i< array.length; i++) {
+            htmlElement.removeChild(array[i]);
+        }
+    }
+    const pTag = document.createElement("p");
+    pTag.innerHTML = string;
+    htmlElement.appendChild(pTag);
+    
 };
 
 htmlGenerator('Party Time.', partyHeader);
