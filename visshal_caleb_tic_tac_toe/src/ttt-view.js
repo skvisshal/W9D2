1856f60aac0308f955f1649abe53edf4cc08d944
Visshal Suresh <skvisshal@gmail.com>
2021-11-30 14:06:45 -0500

    initial View and Setup Board

--- visshal_caleb_tic_tac_toe/src/ttt-view.js
+++ visshal_caleb_tic_tac_toe/src/ttt-view.js
@@ -1,7 +1,19 @@
 class View {
-  constructor(game, el) {}
+  constructor(game, el) {
+    this.game = game;
+    this.el = el;
+    this.setupBoard();
+  }
 
-  setupBoard() {}
+  setupBoard() {
+    const list = document.createElement("ul");
+    for(let i = 0; i < 3; i++) {
+      for(let j =0; j < 3; j++) {
+        const ele = list.appendChild(document.createElement("li"));
+        ele.dataset.pos = `[${i},${j}]`;
+      }
+    }
+  }
   
   bindEvents() {}
 
