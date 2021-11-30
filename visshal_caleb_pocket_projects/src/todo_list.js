030e6850b6185bce70f01def823586ae9e8939ca
Visshal Suresh <skvisshal@gmail.com>
2021-11-30 17:33:21 -0500

    phase 3 add form

--- visshal_caleb_pocket_projects/src/todo_list.js
+++ visshal_caleb_pocket_projects/src/todo_list.js
@@ -1,2 +1,27 @@
+const todos = document.querySelector(".todos");
+const addTodos = document.querySelector(".add-todo-form");
+const todosArr = [];
 
+const addTodo = () => {
+    const value = document.querySelectot("[name=add-todo]").value;
+    const todo = {
+        value: value,
+        done: false
+    };
+
+    todosArr.push(todo);
+    addTodos.reset();
+};
+
+const populateList = (todosArr) => {
+    for(let i =0; i < todosArr.length; i++) {
+        todosArr[i] = `
+            <li>
+                <label for="listItem${i}">${todosArr[i]}</label>
+                <input type="checkbox">
+            </li>
+        `;
+    }
+    todos.innerHTML = todosArr.join('');
+};
 
